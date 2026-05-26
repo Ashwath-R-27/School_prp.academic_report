1bae3d20c84bf5d2f0533689da1c80e2fc44fc55
Ashwath-R-27 <ashwathr27100@gmail.com>
2026-05-26 14:55:20 +0530

    my changes

--- frontend/app.py
+++ frontend/app.py
@@ -1,14 +1,26 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,url_for
 
 app = Flask(__name__)
 
+def header_div():
+    logo_url=url_for('static', filename='logo.png')
+    header=f""" <div class="header">
+        <div class="logo"><a href="#"><img src="{logo_url}" id="logo"></a></div>
+        <div class="header_content">
+            <div class="header_text_1">SVGV MATRICULATION HIGHER SECONDARY SCHOOL</div>
+        </div>
+    </div>"""
+    return header
+
 @app.route("/")
 def first():
-    return render_template("home.html")
+    header=header_div()
+    return render_template("home.html",header_div=header)
 
 @app.route("/HSC_2026")
 def hscmark():
-    return render_template("hscmarkpg.html")
+    header=header_div()
+    return render_template("hscmarkpg.html",header_div=header)
 
 if __name__ == '__main__':
     app.run(debug=True)
